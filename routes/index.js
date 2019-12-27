a3b44a739b7e1bea0232bf91882934b948f8d938
Shashank Parmar <parmarshashank11@gmail.com>
2019-12-27 15:41:13 +0530

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
+const usersController = require('../controllers/users_controller');
+
+console.log('router loaded');
+
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+router.get('/sign-up', usersController.signUp); //display the sign-in page
+router.get('/sign-in', usersController.signIn); //display the sign-up page
+router.post('/create', usersController.create); //create a new user in the database through the signup form
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
+
+module.exports = router;
